61dc2170971690aea2dbfc828d743f0b74bf8f64
Anand014 <kumar.anand014@gmail.com>
2021-01-19 22:16:14 +0530

    filter logic integrated

--- src/Screens/Homescreen.js
+++ src/Screens/Homescreen.js
@@ -3,15 +3,28 @@ import Axios from "axios";
 import "./Homescreen.css";
 import InfiniteScroll from "react-infinite-scroller";
 import Cards from "../Components/Cards/Cards";
+import { Multiselect } from "multiselect-react-dropdown";
 
 const Homescreen = () => {
   const [cardData, setCardData] = useState();
+  const [option, setOption] = useState({
+    objectArray: [
+      { key: "Male", cat: "Gender" },
+      { key: "Female", cat: "Gender" },
+      { key: "US", cat: "nationality" },
+      { key: "DK", cat: "nationality" },
+      { key: "FR", cat: "nationality" },
+      { key: "GB", cat: "nationality" },
+    ],
+    selectedValues: [{ key: "Male", cat: "Gender" }],
+  });
 
   useEffect(() => {
     try {
       Axios.get("https://randomuser.me/api/?results=100&nat=us,dk,fr,gb")
         .then((res) => {
           setCardData(res.data.results);
+          console.log(res.data.results);
         })
         .catch((err) => {
           window.alert("Fetching api data failed");
@@ -28,41 +41,81 @@ const Homescreen = () => {
   const loadFunc = () => {
     //scroll 10
   };
-  return (
-    <InfiniteScroll
-      pageStart={0}
-      threshold={10}
-      loadMore={loadFunc}
-      hasMore={true || false}
-      //   loader={
-      //     <div className="loader" key={0}>
-      //       Loading ...
-      //     </div>
-      //   }
-    >
-      {
-        <div className="showusers">
-          {cardData ? (
-            cardData.map((data, i) => {
-              return (
-                <Cards
-                  key={i}
-                  name={data.name.first}
-                  age={data.dob.age}
-                  gender={data.gender}
-                  dob={data.dob.date}
-                  phone={data.phone}
-                  picture={data.picture}
-                  email={data.email}
-                />
-              );
-            })
-          ) : (
-            <h1>Loading...</h1>
-          )}
-        </div>
+  const onSelect = (selectedList, selectedItem) => {
+    setOption({ ...option, selectedValues: selectedList });
+    console.log(selectedItem);
+    const filteredCard = [];
+    console.log(option.selectedValues, "selected values");
+    cardData.map((data) => {
+      if (
+        selectedItem.cat === "Gender" &&
+        selectedItem.key === "Male" &&
+        data.gender === "male"
+      ) {
+        filteredCard.push(data);
       }
-    </InfiniteScroll>
+      if (
+        selectedItem.cat === "Gender" &&
+        selectedItem.key === "Female" &&
+        data.gender === "female"
+      ) {
+        filteredCard.push(data);
+      }
+      // if (value.cat === "nationality" && value.key === data.nat) {
+      //   filteredCard.push(data);
+      // }
+    });
+    // setCardData(filteredCard);
+    console.log(filteredCard, "all data");
+  };
+  return (
+    <>
+      <div style={{ width: "30%", marginLeft: "16%", marginTop: "1%" }}>
+        <Multiselect
+          placeholder="Filter"
+          options={option.objectArray}
+          groupBy="cat"
+          displayValue="key"
+          selectedValues={option.selectedValues} // Preselected value to persist in dropdown
+          onSelect={onSelect} // Function will trigger on select event
+          // onRemove={onRemove} // Function will trigger on remove event
+        />
+      </div>
+      <InfiniteScroll
+        pageStart={0}
+        threshold={10}
+        loadMore={loadFunc}
+        hasMore={true || false}
+        //   loader={
+        //     <div className="loader" key={0}>
+        //       Loading ...
+        //     </div>
+        //   }
+      >
+        {
+          <div className="showusers">
+            {cardData ? (
+              cardData.map((data, i) => {
+                return (
+                  <Cards
+                    key={i}
+                    name={data.name.first}
+                    age={data.dob.age}
+                    gender={data.gender}
+                    dob={data.dob.date}
+                    phone={data.phone}
+                    picture={data.picture}
+                    email={data.email}
+                  />
+                );
+              })
+            ) : (
+              <h1>Loading...</h1>
+            )}
+          </div>
+        }
+      </InfiniteScroll>
+    </>
   );
 };
 
